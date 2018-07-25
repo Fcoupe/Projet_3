98f1a46417bddb44015ca5eb49cc85059d8ea5fc
Fcoupe <goku.maji13@gmail.com>
2018-07-25 21:49:21 +0200

    Modification index.php séparation du model

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
   // Accées au données
+require ('model/model.php');
 
-        $bdd = new PDO('mysql:host=localhost;dbname=Blog;charset=utf8', 
-          'root', '');
-        $billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
-          . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
-          . ' order by BIL_ID desc');
-        
+$billets = getBillets();
 
 // Affichage
 
