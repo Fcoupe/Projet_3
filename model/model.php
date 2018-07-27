02e94f0c6634e412c854b1dfdca8192d32bcda63
Fcoupe <goku.maji13@gmail.com>
2018-07-27 15:01:19 +0200

    Instanciation de la DataBase

--- model/model.php
+++ model/model.php
@@ -1,9 +1,15 @@
 <?php
-// Renvoie la liste de tous les billets, tier par ID décroisant
+
 function getBillets()
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=Blog;charset=utf8', 
-          'root', '');
-    $billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,' . ' BIL_TITRE as title, BIL_CONTENU as content from T_BILLET' . ' order by BIL_ID desc');
+{// Renvoie la liste de tous les billets, tier par ID décroisant
+	$db = getDb();
+    $billets = $db->query('select BIL_ID as id, BIL_DATE as date,' . ' BIL_TITRE as title, BIL_CONTENU as content from T_BILLET' . ' order by BIL_ID desc');
     return $billets;
+}
+
+function getDb()
+{// Connexion a la DataBase, instancie et renvoie l'objet PDO associer
+	$db = new PDO('mysql:host=localhost;dbname=Blog;charset=utf8', 
+          'root', '');
+	return $db;
 }
