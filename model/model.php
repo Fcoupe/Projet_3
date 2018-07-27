b2c0de495feb0ccf8d6df61e59fbdee10197e68f
Fcoupe <goku.maji13@gmail.com>
2018-07-27 14:54:28 +0200

    Séparation du model et de la vue, ajout template temporaire temp.php

--- model/model.php
+++ model/model.php
@@ -4,6 +4,6 @@ function getBillets()
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=Blog;charset=utf8', 
           'root', '');
-    $billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,' . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET' . ' order by BIL_ID desc');
+    $billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,' . ' BIL_TITRE as title, BIL_CONTENU as content from T_BILLET' . ' order by BIL_ID desc');
     return $billets;
 }
