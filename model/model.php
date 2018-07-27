353184736d23d672ec8bf38f041bc272f794c522
Fcoupe <goku.maji13@gmail.com>
2018-07-27 23:56:02 +0200

    Adaptation model.php en POO

--- model/model.php
+++ model/model.php
@@ -1,43 +1,48 @@
 <?php
 
-function getBillets()
-{// Renvoie la liste de tous les billets, tier par ID décroisant
-	$db = getDb();
-    $billets = $db->query('select BIL_ID as id, BIL_DATE as date_t,' . ' BIL_TITRE as title, BIL_CONTENU as content from T_BILLET' . ' order by BIL_ID desc');
-    return $billets;
-}
+class Model {
 
-function getDb()
-{// Connexion a la DataBase, instancie et renvoie l'objet PDO associer
-	$db = new PDO('mysql:host=localhost;dbname=Blog;charset=utf8', 
-          'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-	return $db;
-}
-
-function getBillet($idBillet)
-{// Renvoie les informations sur un billet
-	$db = getDb();
-	$billet = $db->prepare('SELECT BIL_ID as id,  BIL_DATE as date_t,' . ' BIL_TITRE as title, BIL_CONTENU as content from T_BILLET' . ' WHERE BIL_ID = ?');
-	$billet->execute(array($idBillet));
-
-	if ($billet->rowCount() == 1)
-	{
-		return $billet->fetch(); // Accés a la 1er ligne de resultat
+	public function getBillets()
+	{// Renvoie la liste de tous les billets, tier par ID décroisant
+		$db = $this->getDb();
+	    $billets = $db->query('select BIL_ID as id, BIL_DATE as date_t,' . ' BIL_TITRE as title, BIL_CONTENU as content from T_BILLET' . ' order by BIL_ID desc');
+	    return $billets;
 	}
 
-	else 
-	{
-		throw new Exception ("Aucun Billet ne correspond a l'identifiant '$idbillet'"); 
-	}
-}
+		
 
-function getComments($idBillet)
-{
-	$db = getDb();
-	$comments = $db->prepare('select COM_ID as id, COM_DATE as date_t,'
-    . ' COM_AUTEUR as author, COM_CONTENU as content from T_COM'
-    . ' where BIL_ID=?');
-    $comments->execute(array($idBillet));
-    return $comments;
+	public function getBillet($idBillet)
+	{// Renvoie les informations sur un billet
+		$db = $this->getDb();
+		$billet = $db->prepare('SELECT BIL_ID as id,  BIL_DATE as date_t,' . ' BIL_TITRE as title, BIL_CONTENU as content from T_BILLET' . ' WHERE BIL_ID = ?');
+		$billet->execute(array($idBillet));
+
+		if ($billet->rowCount() == 1)
+		{
+			return $billet->fetch(); // Accés a la 1er ligne de resultat
+		}
+
+		else 
+		{
+			throw new Exception ("Aucun Billet ne correspond a l'identifiant '$idbillet'"); 
+		}
+	}
+
+	public function getComments($idBillet)
+	{
+		$db = $this->getDb();
+		$comments = $db->prepare('select COM_ID as id, COM_DATE as date_t,'
+	    . ' COM_AUTEUR as author, COM_CONTENU as content from T_COM'
+	    . ' where BIL_ID=?');
+	    $comments->execute(array($idBillet));
+	    return $comments;
+	}
+	private function getDb()
+	{// Connexion a la DataBase, instancie et renvoie l'objet PDO associer
+		$db = new PDO('mysql:host=localhost;dbname=Blog;charset=utf8', 
+	          'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	
+		return $db;
+	}
 }
 
