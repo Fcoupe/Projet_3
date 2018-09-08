cafdad3a43f50bd3afbdb4509da7660616046060
Fcoupe <goku.maji13@gmail.com>
2018-09-08 16:27:00 +0200

    Ajout de commentaire

--- controller/controller_menu.php
+++ controller/controller_menu.php
@@ -1,22 +1,22 @@
-<?php
+<?php  		// Controlleur Menu
 require_once ('view/view.php');
 
 class controllerMenu 
 {
 	public function aPropos()
-	{
+	{		// Création de la vue A propos
 		$view = new view("apropos");
 		$view->generate(array());
 	}
 
 	public function contact()
-	{
+	{		// Création de la vue Contact
 		$view = new view("contact");
 		$view->generate(array());
 	}
 
 	public function administrator()
-	{
+	{		// Création de la vue Administrator
 		$view = new view("pass");
 		$view->generate(array());
 	}
