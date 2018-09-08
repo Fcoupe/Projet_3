26aab7286c7fdf357f851281418d39e14c4b5826
Fcoupe <goku.maji13@gmail.com>
2018-09-08 16:25:05 +0200

    Ajout de commentaire

--- controller/controller_home.php
+++ controller/controller_home.php
@@ -1,4 +1,4 @@
-<?php
+<?php 		// Controlleur Page d'accueil
 require_once ('model/billet.php');
 require_once ('view/view.php');
 
@@ -7,12 +7,12 @@ class controllerHome
 	private $billet;
 
 	public function __construct()
-	{
+	{		// Instaciation de la fonction Billet
 		$this->billet = new Billet();
 	}
 
 	public function home($page)
-	{
+	{		// Récuperation des donnés et création de la vue 
 		$bilMax = $this->billet->numBil();
 		$bilLimite = 3;
 		$numberPage = ceil(intval($bilMax['numBil']) / $bilLimite);
