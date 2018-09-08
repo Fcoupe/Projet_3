d506b181336e1d79db08c56e2fde098157b23e43
Fcoupe <goku.maji13@gmail.com>
2018-09-08 16:18:54 +0200

    modification des redirection et ajout de commentaires

--- controller/controller_billet.php
+++ controller/controller_billet.php
@@ -1,4 +1,4 @@
-<?php
+<?php 		// Controulleur des fonctions Billets
 
 require_once ('model/billet.php');
 require_once ('model/comments.php');
@@ -10,13 +10,13 @@ class controllerBillet
 	private $comments;
 
 	public function __construct()
-	{
+	{		// instanciation des fonctions
 		$this->billet = new Billet();
 		$this->comments = new Comments();
 	}
 
 	public function billet($idBillet)
-	{
+	{		// Recuperation des donnée Billet et création de la vue 
 		$billet = $this->billet->getBillet($idBillet);
 		$comments = $this->comments->getComments($idBillet);
 		$view = new view('billet');
@@ -26,34 +26,34 @@ class controllerBillet
 	
 
 	public function addBil ($title, $content)
-	{
+	{		// Ajout de Billet et redirection
 		$this->billet->addBillet($title, $content);
-		header('Location: index.php?action=passView');
+		header('Location: index.php?action=adminPanel');
 		
 	}
 
 	public function updateView($idBillet)
-	{
+	{		// Recuperation données billet et création de la vue
 		$billet = $this->billet->getBillet($idBillet);
 		$view = new view("updateView");
 		$view->generate(array('billet' => $billet));
 	}
 
 	public function updateBil($title, $content, $idBillet)
-	{
+	{		// Modification du billet et redirection
 		$this->billet->updateBills($title, $content, $idBillet);
-		header('Location: index.php?action=passView');
+		header('Location: index.php?action=adminPanel');
 
 	}
 
 	public function delete($idBillet)
-	{
+	{		// Suppression du Billet et redirection
 		$this->billet->deleteBills($idBillet);
 		header('Location: index.php?action=adminPanel');
 	}
 
 	public function billetAdmin($idBillet)
-	{
+	{		// Récuperation données Billet et création de la vue
 		$billet = $this->billet->getBillet($idBillet);
 		$comments = $this->comments->getComments($idBillet);
 		$view = new view('billetAdmin');
